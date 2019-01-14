60ea13872c243fe7c2a9c89dfa5762729e3e5301
Gabriel Hernandez <gabriel.hernandez.condor@gmail.com>
2019-01-14 12:08:24 -0600

    Envio de push y borrado de tokens viejos
    Signed-off-by: Gabriel Hernandez <gabriel.hernandez.condor@gmail.com>

---
+++ server/push.js
@@ -0,0 +1,53 @@
+
+const fs = require('fs');
+
+const urlsafeBase64 = require('urlsafe-base64');
+const vapid = require('./vapid.json');
+
+const webpush = require('web-push');
+
+webpush.setVapidDetails(
+    'mailto:gabriel.hernandez.condor@gmail.com',
+    vapid.publicKey,
+    vapid.privateKey
+  );
+
+let suscripciones = require('./subs-db.json');
+
+
+
+
+module.exports.getKey = () => {
+    return urlsafeBase64.decode(vapid.publicKey);
+};
+
+module.exports.addSubscription = (suscripcion) => {
+    suscripciones.push(suscripcion);
+    
+    fs.writeFileSync(`${__dirname}/subs-db.json`,JSON.stringify(suscripciones))
+}
+
+module.exports.sendPush = (post) => {
+
+    console.log('MANDANDOO PUSHES');
+
+    const notificacionesEnviadas = [];
+
+    suscripciones.forEach((suscripcion,i) => {
+        const pushProm =  webpush.sendNotification(suscripcion,JSON.stringify(post))
+        .then( console.log('Notificacion enviada'))
+        .catch( err => {
+            console.log('Notificacion fallo');
+            if( err.statusCode === 410){ // GONE ya no existe
+                suscripciones[i].borrar = true;
+            }
+        });
+
+        notificacionesEnviadas.push(pushProm);
+    });
+
+    Promise.all(notificacionesEnviadas).then( ()=> {
+        suscripciones = suscripciones.filter( subs => !subs.borrar );
+        fs.writeFileSync(`${__dirname}/subs-db.json`,JSON.stringify(suscripciones));
+    } )
+}
